84c6727a187e5f98aec6dbef828a9483402e569a
Richie <TheMadMaker2@protonmail.com>
2021-08-03 13:48:36 -0400

    basicly re wrote evreything it was so bad

--- date.sh
+++ date.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-
-Sun=3
-
-for i in {1..365}; do 
-
-  First="$(date +%d -d "+$i days")"
+year=$(date +%Y)
+for i in {1..365393}; do
+  # Genrates the mounth eg September(2021)
+  Mounth="$(date "+%B(%Y)" -d "+$i days")"
+  #genrated the day mount and date eg Tuesday_08/03
   Day="$(date +%A_%m/%d -d "+$i days") "
-
-  if [ $First == 01 ]; then
-    echo $(date "+%B(%Y)" -d "+$i days")
-    echo " "
-    echo " "
+  #Updates the year varubual
+  if [[ "$Mounth" != *$year* ]]; then
+    year=$((year + 1))
   fi
-
-  echo "$Day"
-
-  if [[ "$i" -eq "$Sun" ]]; then
-  echo " "
-  Sun=$((Sun + 7)) 
+  #Sets the out put file
+  out=./output/$year.csv 
+  
+  #Prints the month and info for the mounth
+  if [[ "$Day" == */01* ]]; then
+    {
+      echo " "
+      echo "$Mounth,Total # Of Calls:,Notes"
+      echo " "
+    } >> "$out"
   fi
-
+  
+  #Prints the day
+  echo "$Day" >> "$out"
+ 
+  #adds a blank line evrey sunday
+  if [[ "$Day" == Sunday* ]]; then
+      echo " " >> "$out"
+  fi
 done
-
-
-
